ff6194e7d701ed101bd56379d867ebf16f86e276
Dennis Shevtsov <dennisshevtsov@example.com>
2022-12-03 12:18:19 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const html = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new html({
+      title: 'IdentityServer Sample',
+    }),
+  ]
+};
